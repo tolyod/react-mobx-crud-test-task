a005f7c4d35e824b109a246309d07bd6e7136ac1
Anatoliy Poloz <anatoliy.poloz@gmail.com>
2017-12-05 05:45:53 +0300

    fix item index when filter used

--- src/components/ListMates.jsx
+++ src/components/ListMates.jsx
@@ -42,10 +42,15 @@ class ListMates extends React.Component {
 
   render () {
     const itemsList = this.state.mates.slice().map ( (value, index) => {
+
+      const mateIndex = matesStore.matesState.slice().findIndex( (el, index ) => {
+        return (el.name.first == value.name.first) && (el.name.last == value.name.last);
+      } );
+      
       return <MateRowItem
         key={`mateItem_${index}`}
         value={value}
-        mateID={index}/>
+        mateID={mateIndex}/>
     });
 
     return (
