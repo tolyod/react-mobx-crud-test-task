56549a54faea4f6241c824cb44814b5710e3f2bc
Anatoliy Poloz <anatoliy.poloz@gmail.com>
2017-12-05 05:21:06 +0300

    filter added, type fixed, rerun updated cdn version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useStrict } from "mobx";
 import CreateMate from './components/CreateMate';
 import EditMate from './components/EditMate';
